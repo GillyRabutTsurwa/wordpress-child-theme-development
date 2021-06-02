d92daff3e85ed10c72b5f8caadf58a965047f230
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2021-06-02 15:07:40 -0400

    Pluggable Functions - i learnt about pluggable functions, as well as how to use them - check the notes.md file for more information... - ...and the child theme's functions.php file for even more information

--- notes.md
+++ notes.md
@@ -1,22 +1,7 @@
 ## Ce que j'ai appris
-- en gros, l'objectif c'était faire WordPress inclure nos fichiers CSS enfant et parent, comme approprié 
-- on s'est abordé la surface des actions en WordPress
-- on a brièvement étudié la fonction important: ```wp_enqueue_scripts()```
-- modifier les styles existants
-  - pour modifier les styles existantes, le meilleur moyen c'est de copier les styles, aussi que la selecteur, de l'inspecteur; que c'est soit chrome, firefox etc.
-  - coller les styles ici (vscode ou là où que l'on utilise), et les modifier comme desiré  
-- créer les nouvelles styles, en crééant une nouvelle selecteur qui va cibler notre élément
-  - sur WordPress, c'est processus est un peu compliqué, mais rien de trop difficile. il me faut juste y habituer
-  
-<br>
+[link-pluggable-functions]: https://codex.wordpress.org/Pluggable_Functions
 
+- i learnt what a [pluggable function][link-pluggable-functions] is
+-  i learnt how to use them in our child theme to add, remove, and manipulate content as desired
+-  as of right now, at the stage i'm in with this course, and repository, it seems that the use of pluggable functions will be very common
 
-- je prends cette occasion pour prendre la premier defi du cours (nommé "Add Flair")
-- l'objectif c'est de styler d'avantage, les pages, postes, etc, en utilisant ce que l'on a appris
-
-<br>
-
-**Qq'chose important à remarquer:**
-- Au debut, mes styles css ne se rendait pas à WordPress à cause des complications des caches. 
-- Mais, when I do a hard refresh, instead of the normal refresh, I can see my styles getting applied.
-- Alors, The solution was to do a hard refresh, pour que WordPress peut démarrer avec une nouvelle cache vide
--- themes/twentynineteen-child/functions.php
+++ themes/twentynineteen-child/functions.php
@@ -2,26 +2,48 @@
 function my_theme_enqueue_styles() {
     $parent_handle = "parent-style";
     $child_handle = "child-style";
-    /**
-     * NOTE: cette fonction intérieur (dessous), "wp_enqueue_style" est utilisée pour inclure un fichier css particulier 
-     * on utilise cette fonction pour ajouter est activer le fichier css qui vient de notre thème parent: twentynineteen
-     * QUESTION: "parent-style" c'est quoi? Il dit que c'est le nom; le nom de quoi?
-     * 
-     * get_template_directory_uri() charge notre thème parent
-     * ensuite, l'appendage "/style.css" trouve ce fichier-ci dans notre thème parent
-     * voici comment charger et applique les styles venant du thème parent
-     */
     wp_enqueue_style($parent_handle, get_template_directory_uri() . "/style.css");
-    /**
-     * NOTE: cette deuxième fonction nous permet d'écrire nos propre styles pour notre thème d'enfant
-     * QUESTION: À nouveau, c'est quoi "child-style"
-     * 
-     * en plus, ce code dessous est écrit pour que le fichier css du thème parent se charge AVANT celui de notre thème enfant
-     * l'argument array("$parent_handle") est responsable pour cette foncionalité
-     * 
-     * et enfin, l'argument wp_get_theme()->get("Version") et utilisé pour gérer les issus potential des mise en cache
-     */
     wp_enqueue_style($child_handle, get_stylesheet_directory_uri() . "/style.css", array($parent_handle), wp_get_theme()->get("Version"));
 }
 
 add_action("wp_enqueue_scripts", "my_theme_enqueue_styles");
+
+//NEW: 
+/**
+ * this function was that we are targetting is found in twentynineteen/inc/template-tags.php
+ * 1 - this type of function is one we can copy to our child theme, and the declaration of that very same function in the child theme will overwrite the original declaration of it in the parent theme
+ * 2- for the function in the parent theme, its existance is verified by an if statement.
+ * these are two very important things to keep in mind when it comes to working with the functions.php file
+ * such functions are known as pluggable functions
+ * link for more information on them will be on my notes.md file
+ */
+
+/**
+ * NOTE:
+ * so, to clarify: even though this same function is in our parent theme
+ * ...this one here, in the child theme, will be the one that gets used
+ * the one in the parent theme will get overwritten
+ */
+
+/**
+ * for this function, i'm just changing the pagination text of the post page
+ * i'm changing from "New Posts" and "Older Posts" to "Vipya" and "Vya Zamani"
+ */
+function twentynineteen_the_posts_navigation() {
+    the_posts_pagination(
+        array(
+            'mid_size'  => 2,
+            'prev_text' => sprintf(
+                '%s <span class="nav-prev-text">%s</span>',
+                twentynineteen_get_icon_svg( 'chevron_left', 22 ),
+                __( 'Vipya', 'twentynineteen' )
+            ),
+            'next_text' => sprintf(
+                '<span class="nav-next-text">%s</span> %s',
+                __( 'Vya Zamani', 'twentynineteen' ),
+                twentynineteen_get_icon_svg( 'chevron_right', 22 )
+            ),
+        )
+    );
+}
+ 
